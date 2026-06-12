e0a82666922627cd130dcb20f4df3bb8f3bda692
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-06-12 10:15:44 -0600

    feat: Hacer cambios en la clase Evaluacion

--- src/main/java/ni/edu/uam/modelo/Evaluacion.java
+++ src/main/java/ni/edu/uam/modelo/Evaluacion.java
@@ -12,24 +12,35 @@ import java.util.Collection;
 @Entity
 @Getter
 @Setter
-@View(members = "fecha,candidato;puntajeTotal;respuestas")
+@View(members = "candidato;" + "idEvaluacion, fechaInicio, estado, notaFinal;" + "respuestas") //Organiza como se ve en la pantalla
 public class Evaluacion {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Hidden
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ReadOnly
+    private long idEvaluacion;
 
-    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
-    private LocalDate fecha;
-
-    @ManyToOne(fetch=FetchType.LAZY, optional=false)
-    @DescriptionsList
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @DescriptionsList //Muestra un combo box con los candidatos
     private Candidato candidato;
 
-    @ReadOnly
-    private int puntajeTotal;
+    @DefaultValueCalculator(org.openxava.calculators.CurrentLocalDateCalculator.class)
+    private LocalDate fechaInicio;
 
-    @OneToMany(mappedBy="evaluacion", cascade=CascadeType.ALL)
-    @ListProperties("pregunta.numeroPregunta, letraSeleccionada, esCorrecta")
+    @Column(length = 20)
+    private String estado = "Pendiente"; //Estado inicial
+
+    @ReadOnly //Se calcula mediante la accion, el usuario no la edita a mano
+    private double notaFinal;
+
+    @OneToMany(mappedBy = "evaluacion", cascade = CascadeType.ALL)
     private Collection<RespuestaDetalle> respuestas;
+
+    //Metodos definidos en el enterprise architect
+    public void iniciarEvaluacion(){
+        this.estado = "En progreso...";
+    }
+
+    public void finalizarEvaluacion(){
+        this.estado = "Finalizado";
+    }
 }
